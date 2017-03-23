b1328fec12e258f360dcdae8b5b6321a2075e907
Rotimi Opeke <rotimi.opeke@gmail.com>
2017-03-23 19:29:18 +0000

    First commit on this really simple application

--- flask_app.py
+++ flask_app.py
@@ -1,35 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template, request, url_for, redirect
-from flask.ext.sqlalchemy import SQLAlchemy
-
+from flask import Flask
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="opeker",
-    password="1stneverfollows",
-    hostname="opeker.mysql.pythonanywhere-services.com",
-    databasename="opeker$comments",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-
-db = SQLAlchemy(app)
-
-comments = []
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-    if request.method == "GET":
-        return render_template("index.html", comments=comments)
-
-    comments.append(request.form["contents"])
-    return redirect(url_for('index'))
-
-@app.route('/about')
-def about():
-    return "This is my non-existent 'about' page"
+@app.route('/')
+def hello_world():
+    return 'This is Rotimi\'s test app'
 
