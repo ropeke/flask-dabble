f41d45913407621eeae76d7ed3a8f46c792ad204
Rotimi Opeke <rotimi.opeke@gmail.com>
2017-03-24 04:39:12 +0000

    included the main app file

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"]
 
 @app.route('/')
 def hello_world():
-    return 'This is Rotimi\'s test app'
+    return render_template('main_page.html')
 
