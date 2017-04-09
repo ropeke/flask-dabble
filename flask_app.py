42cc24267fa415581c363155cbca508ababd5d2f
Rotimi Opeke <rotimi.opeke@gmail.com>
2017-04-09 06:28:31 +0000

    added the backend for the database

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,20 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, redirect, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"]
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+            username="opeker",
+            password="gbolojo23db",
+            hostname="opeker.mysql.pythonanywhere-services.com",
+            databasename="opeker$comments",)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
 
 comments = []
 
