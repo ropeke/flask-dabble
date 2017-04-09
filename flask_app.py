245f4ff7eb509591cdd0b929e05cb19393f7a50f
Rotimi Opeke <rotimi.opeke@gmail.com>
2017-04-09 08:14:12 +0000

    added html template, but not properly linking with js and css. Also need to figure out how to get this to link to root

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,9 @@
 
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["DEBUG"]
+app.config["DEBUG"] == True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
             username="opeker",
@@ -25,8 +23,7 @@ class Comment(db.Model):
     content = db.Column(db.String(4096))
 
 
-
-@app.route('/', methods=["GET","POST"])
+@app.route('/list', methods=["GET","POST"])
 def index():
     if request.method == "GET":
         return render_template('main_page.html', comments=Comment.query.all())
@@ -36,3 +33,6 @@ def index():
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.route('/lmao', methods=["GET"])
+def home_page():
+    return render_template('index.html')
